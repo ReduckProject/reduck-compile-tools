a63c589a5aa4a6c26741fcb69f33aa119e56ea40
ReduckProject <reduckproject@gmail.com>
2024-07-16 18:03:52 +0800

    update # optimize code

--- src/main/java/net/reduck/compile/tools/IgnoreDuringCompileProcessor.java
+++ src/main/java/net/reduck/compile/tools/IgnoreDuringCompileProcessor.java
@@ -1,75 +1,5 @@
-//package net.reduck.compile.tools;
-//
-//import javax.annotation.processing.*;
-//import javax.lang.model.SourceVersion;
-//import javax.lang.model.element.Element;
-//import javax.lang.model.element.ExecutableElement;
-//import javax.lang.model.element.TypeElement;
-//import javax.tools.Diagnostic;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Set;
-//import javassist.*;
-//
-///**
-// * @author Reduck
-// * @since 2024/7/16 10:43
-// */
-//@SupportedAnnotationTypes("net.reduck.compile.tools.IgnoreDuringCompile")
-//@SupportedSourceVersion(SourceVersion.RELEASE_8)
-//public class IgnoreDuringCompileProcessor extends AbstractProcessor {
-//
-//    private final List<String> methodsToRemove = new ArrayList<>();
-//
-//    @Override
-//    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-//        for (Element element : roundEnv.getElementsAnnotatedWith(IgnoreDuringCompile.class)) {
-//            if (element instanceof ExecutableElement) {
-//                TypeElement classElement = (TypeElement) element.getEnclosingElement();
-//                String className = classElement.getQualifiedName().toString();
-//                String methodName = element.getSimpleName().toString();
-//                methodsToRemove.add(className + "#" + methodName);
-//
-//                processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Method " + element + " will be removed.", element);
-//            }
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public void init(ProcessingEnvironment processingEnv) {
-//        super.init(processingEnv);
-//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-//            try {
-//                processClasses();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }));
-//    }
-//
-//    private void processClasses() throws NotFoundException, CannotCompileException, IOException {
-//        ClassPool pool = ClassPool.getDefault();
-//
-//        for (String methodInfo : methodsToRemove) {
-//            String[] parts = methodInfo.split("#");
-//            String className = parts[0];
-//            String methodName = parts[1];
-//
-//            CtClass ctClass = pool.get(className);
-//            CtMethod ctMethod = ctClass.getDeclaredMethod(methodName);
-//            ctClass.removeMethod(ctMethod);
-//            ctClass.writeFile();
-//        }
-//    }
-//}
-
 package net.reduck.compile.tools;
 
-import com.sun.source.tree.ClassTree;
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.MethodTree;
 import com.sun.source.util.Trees;
 import com.sun.tools.javac.api.JavacTrees;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
@@ -79,11 +9,7 @@ import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
 import com.sun.tools.javac.util.Names;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
@@ -91,6 +17,10 @@ import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
 
+/**
+ * @author Reduck
+ * @since 2024/7/16 10:42
+ */
 @SupportedAnnotationTypes("net.reduck.compile.tools.IgnoreDuringCompile")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class IgnoreDuringCompileProcessor extends AbstractProcessor {
